9ef7072da0170c61b91bff9cf6deb51af1ea3052
jmuhire128 <jmuhire128@mail.com>
2023-06-15 07:15:24 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,22 @@
+/*
+ *Author: Muhire JB
+ *File: 5-printf
+ *Description: printing with printf
+*/
+
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: the function main this case do:
+ * Prints a string
+ *describe the parameter
+ *Return: always 0
+ */
+int main(void)
+{
+/* printf return zero if success */
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
