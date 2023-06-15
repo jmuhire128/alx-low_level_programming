15d2d20d9bcc8bb4c5eaac1aa754d28873b6ba3c
jmuhire128 <jmuhire128@mail.com>
2023-06-15 08:25:25 -0700

    using write(2, .... 60) funct

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+/*
+ *Write a C program that prints
+ */
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Description: This program will prints
+ *without error using Neither printf nor puts
+ *Return: 1 if success
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+return (1);
+}
