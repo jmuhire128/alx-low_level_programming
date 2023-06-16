65759a621f5938150f0c4bdfa4a06a3866bf07f8
jmuhire128 <jmuhire128@mail.com>
2023-06-16 10:58:30 -0700

    digits without char var

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 /**
  * main - print numbers
- * single digits by char
+ * single digits by char without char variable
  * using putchar
  *
  * Return: always 0 if success
  */
 int main(void)
 {
-	char digit;
+	int digit;
 
-	for (digit = '0'; digit <= '9'; digit++)
+	for (digit = 0; digit < 10; digit++)
 	{
-		putchar(digit);
+		putchar((digit % 10) + '0');
 	}
 	putchar('\n');
 	return (0);
