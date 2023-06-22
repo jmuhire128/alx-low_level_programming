f383e12f8f4ba188a912723b5fab326e6199790f
jmuhire128 <jmuhire128@mail.com>
2023-06-22 05:24:14 -0700

    printing line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,12 +11,15 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = n; i >= 0; i--)
+	if (n <= 0)
 	{
-		if (n != 0)
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
 		{
 			_putchar('_');
 		}
 	}
-	_putchar('\n');
 }
