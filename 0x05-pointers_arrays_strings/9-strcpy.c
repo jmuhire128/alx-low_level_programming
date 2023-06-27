3dffb8e522b57b48bd2baf02d4f57c96139df4db
jmuhire128 <jmuhire128@mail.com>
2023-06-27 12:40:16 -0700

    copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcpy - function that copies string 2 to 1
+ * @dest: final string
+ * @src: string to copy
+ *
+ * Return: a character
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	for (; i < src[i]; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
