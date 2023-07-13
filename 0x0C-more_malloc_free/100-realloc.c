94d5203b4ca07969e805252c6255ed9b569be935
jmuhire128 <jmuhire128@mail.com>
2023-07-13 13:22:37 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -18,19 +18,19 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size > old_size)
 	{
 		s = malloc(new_size * sizeof(char));
+
 		free(ptr);
 		return (s);
 	}
 
 	if (new_size == old_size)
 	{
-		return (s);
+		return (ptr);
 	}
 
 	if (ptr == NULL)
 	{
 		s = malloc(new_size * sizeof(char));
-		free(ptr);
 		return (s);
 	}
 
