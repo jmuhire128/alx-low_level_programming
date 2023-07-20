688112f0b84efbd1e3bfb8be652fff7c2c87da8c
jmuhire128 <jmuhire128@mail.com>
2023-07-20 03:00:31 -0700

    sum of all

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - make a sum of all variables
+ * @n: args number og argument
+ *
+ * Return: int
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum;
+	va_list numbers;
+
+	va_start(numbers, n);
+
+	sum = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(numbers, int);
+	}
+
+	va_end(numbers);
+	return (sum);
+}
